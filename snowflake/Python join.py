01030c2a177e781ddfc46c318bd105673d50fcff
patric <Patric.Hamilton@gmail.com>
2022-05-11 07:15:14 +0000

    no changes

--- snowflake/Python join.py
+++ snowflake/Python join.py
@@ -2,7 +2,7 @@
 import pandas as pd
 snowclass = SAS.sd2df('mysnow.class') 
 snowclassfit = SAS.sd2df('mysnow.classfit')
-
+/*En beskrivning*/
 inner_join_df= pd.merge(snowclass, snowclassfit, on='Name', how='inner')
 
 snowclassjoin = SAS.df2sd(inner_join_df, 'work.snowclassjoin')
